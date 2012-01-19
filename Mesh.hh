98ca1ed3dcf1fd444c49507c68abe0dd2a806b2a
Jeffrey Johnson (jnjohnson <Jeffrey Johnson (jnjohnson@lbl.gov)>
2012-01-19 09:14:25 -0800

    Added basic Mesh descriptor.

---
+++ Mesh.hh
@@ -0,0 +1,30 @@
+#ifndef MESH_HH
+#define MESH_HH
+
+#include <vector>
+typedef double Real;
+
+//! \class Mesh - A basic descriptor class for a topologically-consistent 
+//! arbitrary poly(gonal/hedral) mesh.
+class Mesh
+{
+  public:
+
+  //! An array of (Dimension*numNodes) values containing components of 
+  //! node positions. The components are stored in node-major order and 
+  //! the 0th component of the ith node appears in nodes[Dimension*i].
+  std::vector<Real> nodes;
+
+  //! This two-dimensional array defines the cell-face topology of the 
+  //! mesh. A cell has an arbitrary number of faces in 2D and 3D.
+  //! cells[i][j] gives the index of the jth face of the ith cell.
+  std::vector<std::vector<int> > cells;
+
+  //! This two-dimensional array defines the topology of the faces of the 
+  //! mesh. A face has an arbitrary number of nodes in 3D and 2 nodes in 2D. 
+  //! faces[i][j] gives the index of the jth node of the ith face.
+  std::vector<std::vector<int> > faces;
+};
+
+
+#endif
--- PLC.hh
+++ PLC.hh
@@ -6,12 +6,11 @@ typedef double Real;
 
 //! \class PLC - A Piecewise Linear Complex in 3D, or a Planar Straight Line 
 //! Graph (PSLG) in 2D.
-template <int Dimension>
 class PLC
 {
   public:
 
-  //! An array of (Dimension*numPoints) values containing components of 
+  //! An array of (Dimension*numNodes) values containing components of 
   //! node positions. The components are stored in node-major order and 
   //! the 0th component of the ith node appears in nodes[Dimension*i].
   std::vector<Real> nodes;
@@ -22,7 +21,7 @@ class PLC
   //! node of the ith face.
   std::vector<std::vector<int> > faces;
 
-  //! This array of size (Dimension*numPoints) contains components of 
+  //! This array of size (Dimension*numHoles) contains components of 
   //! points identifying holes to be subtracted from the volume enclosed by 
   //! the PLC. Regions of the PLC containing a hole will not contain any 
   //! cells in their corresponding mesh. The components are stored in 
