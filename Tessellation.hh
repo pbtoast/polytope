239faf58bf2a51c64b0d6dc629bd8dca7e097146
Firstname Lastname <firstname.lastname@example.net>
2012-01-22 23:19:21 -0800

    Slight interface changes in our generic polytope interface classes.

--- Tessellation.hh
+++ Tessellation.hh
@@ -8,6 +8,7 @@ namespace polytope
 
 //! \class Mesh - A basic descriptor class for a topologically-consistent 
 //! arbitrary poly(gonal/hedral) mesh.
+template<typename Real>
 class Tessellation
 {
   public:
@@ -20,12 +21,23 @@ class Tessellation
   //! This two-dimensional array defines the cell-face topology of the 
   //! mesh. A cell has an arbitrary number of faces in 2D and 3D.
   //! cells[i][j] gives the index of the jth face of the ith cell.
+  //! A negative face index indicates the actual face index is the 1's 
+  //! complement of the value (~cells[i][j]) and the face is oriented
+  //! witih an inward pointing normal for cells[i].
   std::vector<std::vector<int> > cells;
 
   //! This two-dimensional array defines the topology of the faces of the 
   //! mesh. A face has an arbitrary number of nodes in 3D and 2 nodes in 2D. 
   //! faces[i][j] gives the index of the jth node of the ith face.
-  std::vector<std::vector<int> > faces;
+  //! Nodes for a given face are arranged counterclockwise around the face
+  //! viewed from the "positive" (outside) direction. 
+  std::vector<std::vector<unsigned> > faces;
+
+  //! An array of cell indices for each face, i.e., the cells that share
+  //! the face.
+  //! For a given cell there will be either 1 or 2 cells -- the cases with 1
+  //! cell indicate a face on a boundary of the tessellation.
+  std::vector<std::vector<unsigned> > faceCells;
 
   private:
 
