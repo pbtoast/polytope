c760292cea4d1eae5e9f6495276c8ed332a9c709
Jeffrey Johnson <jeffjohnson@pobox.com>
2012-01-28 13:34:52 -0800

    Added build flags for Triangle and Tetgen.

--- src/TriangleTessellator.cc
+++ src/TriangleTessellator.cc
@@ -6,7 +6,11 @@
 #include <map>
 
 #include "polytope.hh" // Pulls in ASSERT and TriangleTessellator.hh.
+
+extern "C"
+{
 #include "triangle.h" 
+}
 
 namespace polytope {
 
@@ -24,7 +28,8 @@ TriangleTessellator():
 //------------------------------------------------------------------------------
 template<typename Real>
 TriangleTessellator<Real>::
-~TriangleTessellator() {
+~TriangleTessellator() 
+{
 }
 //------------------------------------------------------------------------------
 
@@ -35,6 +40,38 @@ TriangleTessellator<Real>::
 tessellate(const vector<Real>& points,
            Tessellation<Real>& mesh) const 
 {
+  triangulateio in, delaunay;
+
+  // Define input points.
+  in.numberofpoints = points.size()/2;
+  in.pointlist = new Real[points.size()];
+  copy(points.begin(), points.end(), in.pointlist);
+
+  // No point attributes or markers.
+  in.numberofpointattributes = 0;
+  in.pointattributelist = 0; 
+  in.pointmarkerlist = 0; // No point markers.
+
+  // No segments or holes.
+  in.numberofsegments = 0;
+  in.numberofholes = 0;
+
+  // No regions.
+  in.numberofregions = 0;
+  in.regionlist = 0;
+
+  // Set up the structure for the triangulation.
+  delaunay.pointlist = 0;
+  delaunay.pointattributelist = 0;
+  delaunay.pointmarkerlist = 0;
+  delaunay.trianglelist = 0;
+  delaunay.triangleattributelist = 0;
+  delaunay.neighborlist = 0;
+  delaunay.edgelist = 0;
+  delaunay.edgemarkerlist = 0;
+
+  // Do the triangulation.
+  triangulate((char*)"QzevnB", &in, &delaunay, 0);
 }
 //------------------------------------------------------------------------------
 
