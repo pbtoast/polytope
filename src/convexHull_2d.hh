4c8618c74269aa99b38a8134600947747c42ab25
Mike Owen <mikeowen@llnl.gov>
2012-02-20 21:23:04 -0800

    Moving the Point2 and Point3 classes to their own header since I'm using them in a few places now.

--- src/convexHull_2d.hh
+++ src/convexHull_2d.hh
@@ -12,39 +12,9 @@
 #include <stdint.h>
 
 #include "polytope.hh"
+#include "Point.hh"
 
 namespace polytope {
-
-//------------------------------------------------------------------------------
-// A integer version of the simple 2D point.
-//------------------------------------------------------------------------------
-template<typename UintType>
-struct Point2 {
-  UintType x, y;
-  Point2(): x(0), y(0) {}
-  Point2(const UintType& xi, const UintType& yi): x(xi), y(yi) {}
-  Point2& operator=(const Point2& rhs) { x = rhs.x; y = rhs.y; return *this; }
-  bool operator==(const Point2& rhs) const { return (x == rhs.x and y == rhs.y); }
-  bool operator<(const Point2& rhs) const {
-    return (x < rhs.x                ? true :
-            x == rhs.x and y < rhs.y ? true :
-            false);
-  }
-  template<typename RealType>
-  Point2(const RealType& xi, const RealType& yi, const RealType& dx): 
-    x(static_cast<UintType>(xi/dx + 0.5)),
-    y(static_cast<UintType>(yi/dx + 0.5)) {}
-  template<typename RealType> RealType realx(const RealType& xmin, const RealType& dx) { return static_cast<RealType>(x*dx) + xmin; }
-  template<typename RealType> RealType realy(const RealType& ymin, const RealType& dy) { return static_cast<RealType>(y*dy) + ymin; }
-};
-
-// It's nice being able to print these things.
-template<typename UintType>
-std::ostream&
-operator<<(std::ostream& os, const Point2<UintType>& p) {
-  os << "(" << p.x << " " << p.y << ")";
-  return os;
-}
 
 namespace { // We hide internal functions in an anonymous namespace.
 
