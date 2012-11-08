2af49578faaec1632b97a1ea6baca9b99ed77f57
Mike Owen <mikeowen@llnl.gov>
2012-11-07 23:39:38 -0800

    Experimenting with using Morton ordering and adjacency to build the shared nodes & faces without additional communication.
    Ported the Morton ordering hash coding from Spheral++.

--- src/Point.hh
+++ src/Point.hh
@@ -47,6 +47,7 @@ struct Point2 {
   template<typename RealType> RealType realx(const RealType& xmin, const RealType& dx) const { return static_cast<RealType>(x*dx) + xmin; }
   template<typename RealType> RealType realy(const RealType& ymin, const RealType& dy) const { return static_cast<RealType>(y*dy) + ymin; }
   Point2& operator+=(const Point2& rhs) { x += rhs.x; y += rhs.y; return *this; }
+  Point2& operator-=(const Point2& rhs) { x -= rhs.x; y -= rhs.y; return *this; }
   Point2& operator/=(const UintType& rhs) { x /= rhs; y /= rhs; return *this; }
 };
 
@@ -116,6 +117,7 @@ struct Point3 {
   template<typename RealType> RealType realy(const RealType& ymin, const RealType& dy) const { return static_cast<RealType>(y*dy) + ymin; }
   template<typename RealType> RealType realz(const RealType& zmin, const RealType& dz) const { return static_cast<RealType>(z*dz) + zmin; }
   Point3& operator+=(const Point3& rhs) { x += rhs.x; y += rhs.y; z += rhs.z; return *this; }
+  Point3& operator-=(const Point3& rhs) { x -= rhs.x; y -= rhs.y; z -= rhs.z; return *this; }
   Point3& operator/=(const UintType& rhs) { x /= rhs; y /= rhs; z /= rhs; return *this; }
 };
 
---
+++ src/mortonOrderIndices.hh
@@ -0,0 +1,103 @@
+//-----------------------------------------------------------------------------//
+// mortonOrderIndicies
+//
+// Compute the Morton ordered hashed indicies for the given set of NodeLists.
+// 
+// Algorithm described in
+// Warren & Salmon (1995), Computer Physics Communications, 87, 266-290.
+//
+// Ported from the Spheral++ algorithm.
+//----------------------------------------------------------------------------//
+#ifndef __Polytope_mortonOrderIndices__
+#define __Polytope_mortonOrderIndices__
+
+#include <vector>
+
+#include "polytope.hh"
+#include "KeyTraits.hh"
+
+namespace {
+
+//------------------------------------------------------------------------------
+// Stand alone functions to do the interleaved bit hashing of positions.
+//------------------------------------------------------------------------------
+// 2-D
+inline
+polytope::KeyTraits::Key
+hashPosition(const polytope::Point2<polytope::KeyTraits::Key>& xoff) {
+
+  typedef polytope::KeyTraits::Key Key;
+
+  // Get the bits for each dimension.
+  ASSERT(xoff.x <= polytope::KeyTraits::maxKey1d);
+  ASSERT(xoff.y <= polytope::KeyTraits::maxKey1d);
+
+  // Interleave the bits.
+  Key result = polytope::KeyTraits::zero;
+  Key mask = polytope::KeyTraits::one;
+  for (size_t i = 0; i != polytope::KeyTraits::numbits1d; ++i, mask <<= 1) {
+    result += (((xoff.x & mask) << i) +
+               ((xoff.y & mask) << (i + 1)));
+  }
+
+  // That's it.
+  ASSERT(result <= polytope::KeyTraits::maxKey);
+  return result;
+}
+
+// 3-D
+inline
+polytope::KeyTraits::Key
+hashPosition(const polytope::Point3<polytope::KeyTraits::Key>& xoff) {
+
+  typedef polytope::KeyTraits::Key Key;
+
+  // Get the bits for each dimension.
+  ASSERT(xoff.x <= polytope::KeyTraits::maxKey1d);
+  ASSERT(xoff.y <= polytope::KeyTraits::maxKey1d);
+  ASSERT(xoff.z <= polytope::KeyTraits::maxKey1d);
+
+  // Interleave the bits.
+  Key result = polytope::KeyTraits::zero;
+  Key mask = polytope::KeyTraits::one;
+  for (size_t i = 0; i != polytope::KeyTraits::numbits1d; ++i, mask <<= 1) {
+    result += (((xoff.x & mask) << i) +
+               ((xoff.y & mask) << (i + 1)) +
+               ((xoff.z & mask) << (i + 2)));
+  }
+
+  // That's it.
+  ASSERT(result <= polytope::KeyTraits::maxKey);
+  return result;
+}
+
+}
+
+//------------------------------------------------------------------------------
+// Hash the positions into their tree ordered indicies.
+//------------------------------------------------------------------------------
+namespace polytope {
+
+template<typename PointType>
+std::vector<KeyTraits::Key>
+mortonOrderIndices(const std::vector<PointType>& points) {
+
+  typedef typename KeyTraits::Key Key;
+
+  // Prepare the result.
+  const unsigned npoints = points.size();
+  std::vector<KeyTraits::Key> result;
+  result.reserve(npoints);
+
+  // Go over all points.
+  for (typename std::vector<PointType>::const_iterator itr = points.begin();
+       itr != points.end();
+       ++itr) result.push_back(hashPosition(*itr));
+
+  ASSERT(result.size() == npoints);
+  return result;
+}
+
+}
+
+#endif
