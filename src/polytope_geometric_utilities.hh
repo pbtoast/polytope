29f5616ec4bc8ef3d53358537e7569e3774f2f39
Mike Owen <mikeowen@llnl.gov>
2013-01-27 15:06:50 -0800

    Tetgen tessellator passing the most basic tests.

--- src/polytope_geometric_utilities.hh
+++ src/polytope_geometric_utilities.hh
@@ -530,76 +530,33 @@ computeCircumcenter2d(double* A, double* B, double* C, double* X) {
 }
 
 //------------------------------------------------------------------------
-// This function computes the determinant of the 3x3 matrix A with 
-// the given 9 coefficients.
-//------------------------------------------------------------------------
-inline
-double 
-det3(double A11, double A12, double A13,
-     double A21, double A22, double A23,
-     double A31, double A32, double A33) {
-  return A11*(A22*A33-A32*A23) - A12*(A21*A33-A31*A23) + A13*(A21*A31-A31*A22);
-}
-
-//------------------------------------------------------------------------
-// This function computes the determinant of the 4x4 matrix A with 
-// the given 16 coefficients.
-//------------------------------------------------------------------------
-inline
-double 
-det4(double A11, double A12, double A13, double A14,
-     double A21, double A22, double A23, double A24,
-     double A31, double A32, double A33, double A34,
-     double A41, double A42, double A43, double A44) {
-  double det31 = det3(A22, A23, A24,
-                      A32, A33, A34,
-                      A42, A43, A44);
-  double det32 = det3(A21, A23, A24,
-                      A31, A33, A34,
-                      A41, A43, A44);
-  double det33 = det3(A21, A22, A24,
-                      A31, A32, A34,
-                      A41, A42, A44);
-  double det34 = det3(A21, A22, A23,
-                      A31, A32, A33,
-                      A41, A42, A43);
-  return A11*det31 - A12*det32 + A13*det33 - A14*det34;
-}
-
-//------------------------------------------------------------------------
 // This function computes the circumcenter of a tetrahedron with vertices
 // A = (Ax, Ay, Az), B = (Bx, By, Bz), C = (Cx, Cy, Cz), and D = (Dx, Dy, Dz).
 // The result is returned in X.
+// This solution was taken from http://en.wikipedia.org/wiki/Tetrahedron
 //------------------------------------------------------------------------
 inline
 void
 computeCircumcenter3d(double* A, double* B, double* C, double* D, double* X) {
-  // This solution was taken from 
-  // http://mathworld.wolfram.com/Circumsphere.html.
-  double r12 = A[0]*A[0] + A[1]*A[1] + A[2]*A[2];
-  double r22 = B[0]*B[0] + B[1]*B[1] + B[2]*B[2];
-  double r32 = C[0]*C[0] + C[1]*C[1] + C[2]*C[2];
-  double r42 = D[0]*D[0] + D[1]*D[1] + D[2]*D[2];
-  double a = det4(A[0], A[1], A[2], 1.0,
-                  B[0], B[1], B[2], 1.0,
-                  C[0], C[1], C[2], 1.0,
-                  D[0], D[1], D[2], 1.0);
-  double Dx = det4(r12, A[1], A[2], 1.0,
-                   r22, B[1], B[2], 1.0,
-                   r32, C[1], C[2], 1.0,
-                   r42, D[1], D[2], 1.0);
-  double Dy = det4(r12, A[0], A[2], 1.0,
-                   r22, B[0], B[2], 1.0,
-                   r32, C[0], C[2], 1.0,
-                   r42, D[0], D[2], 1.0);
-  double Dz = det4(r12, A[0], A[1], 1.0,
-                   r22, B[0], B[1], 1.0,
-                   r32, C[0], C[1], 1.0,
-                   r42, D[0], D[1], 1.0);
-  POLY_ASSERT(a != 0.0);
-  X[0] = 0.5*Dx/a;
-  X[1] = 0.5*Dy/a;
-  X[2] = 0.5*Dz/a;
+  const double a[3] = {B[0] - A[0], B[1] - A[1], B[2] - A[2]},
+               b[3] = {C[0] - A[0], C[1] - A[1], C[2] - A[2]},
+               c[3] = {D[0] - A[0], D[1] - A[1], D[2] - A[2]};
+  const double a2 = dot<3, double>(a, a),
+               b2 = dot<3, double>(b, b),
+               c2 = dot<3, double>(c, c);
+  double b_cross_c[3], c_cross_a[3], a_cross_b[3];
+  cross<3, double>(b, c, b_cross_c);
+  cross<3, double>(c, a, c_cross_a);
+  cross<3, double>(a, b, a_cross_b);
+  const double denom = 2.0*dot<3, double>(a, b_cross_c);
+  POLY_ASSERT2(denom != 0.0,
+               "A : (" << A[0] << " " << A[1] << " " << A[2] << ")\n"
+               << "B : (" << B[0] << " " << B[1] << " " << B[2] << ")\n"
+               << "C : (" << C[0] << " " << C[1] << " " << C[2] << ")\n"
+               << "D : (" << D[0] << " " << D[1] << " " << D[2] << ")\n");
+  X[0] = A[0] + (a2*b_cross_c[0] + b2*c_cross_a[0] + c2*a_cross_b[0])/denom;
+  X[1] = A[1] + (a2*b_cross_c[1] + b2*c_cross_a[1] + c2*a_cross_b[1])/denom;
+  X[2] = A[2] + (a2*b_cross_c[2] + b2*c_cross_a[2] + c2*a_cross_b[2])/denom;
 }
 
 //------------------------------------------------------------------------------
@@ -657,7 +614,6 @@ computeTetCentroid(double* A, double* B, double* C, double* D, double* X) {
   X[0] = 0.25*(A[0] + B[0] + C[0] + D[0]);
   X[1] = 0.25*(A[1] + B[1] + C[1] + D[1]);
   X[2] = 0.25*(A[2] + B[2] + C[2] + D[2]);
-  X[3] = 0.25*(A[3] + B[3] + C[3] + D[3]);
 }
 
 }
