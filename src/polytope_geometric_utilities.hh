ad9605b97be9e0e184256a78c12a8c5ae798f44b
David Starinshak <starinshak1@llnl.gov>
2013-03-21 14:38:11 -0700

    A few small test set changes

--- src/polytope_geometric_utilities.hh
+++ src/polytope_geometric_utilities.hh
@@ -12,8 +12,49 @@
 #include "polytope_parallel_utilities.hh"
 #endif
 
+// --- These live in predicates.cc
+// Compute the orientation of point c relative to points a and b
+extern double orient2d(double* a, double* b, double* c);
+// Sets h = b*e
+extern int scale_expansion(int elen, double* e, double b, double* h);
+// Sets h = e + b
+extern int grow_expansion(int elen, double* e, double b, double* h);
+
 namespace polytope {
 namespace geometry {
+
+
+//------------------------------------------------------------------------------
+// computeNorm
+//------------------------------------------------------------------------------
+inline
+double
+computeSquareNorm(double* A) {
+  int i = 0;
+  double axax[2], ayay[2], tmp[2];
+  i += scale_expansion(1, &A[0], A[0], axax);
+  i += scale_expansion(1, &A[1], A[1], ayay);
+  i += grow_expansion(1, &axax[1], ayay[1], &tmp[0]);  
+  return tmp[1];
+}
+
+//------------------------------------------------------------------------------
+// Triangle circumcenter
+//------------------------------------------------------------------------------
+// Compute the circumcenter using fast predicates
+inline
+void
+computeCircumcenter(double* A, double* B, double* C, double* X) {
+  double Anorm = computeSquareNorm(A);
+  double Bnorm = computeSquareNorm(B);
+  double Cnorm = computeSquareNorm(C);
+  double D = 2*orient2d(A,B,C);
+  double a0[2] = {Anorm, A[1]},   a1[2] = {A[0], Anorm};
+  double b0[2] = {Bnorm, B[1]},   b1[2] = {B[0], Bnorm};
+  double c0[2] = {Cnorm, C[1]},   c1[2] = {C[0], Cnorm};
+  X[0] = orient2d(a0,b0,c0)/D;
+  X[1] = orient2d(a1,b1,c1)/D;
+} 
 
 //------------------------------------------------------------------------------
 // Distance between points.
@@ -181,8 +222,7 @@ collinear(const RealType* a, const RealType* b, const RealType* c, const RealTyp
     ab[j] /= abmag;
     ac[j] /= acmag;
   }
-  //return std::abs(std::abs(dot<Dimension, RealType>(ab, ac)) - 1.0) < tol;
-  return abs(abs(dot<Dimension, RealType>(ab, ac)) - 1.0) < tol;
+  return std::abs(std::abs(dot<Dimension, RealType>(ab, ac)) - 1.0) < tol;
 }
 
 //------------------------------------------------------------------------------
