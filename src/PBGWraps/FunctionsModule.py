11b38ee269696e4da08aa4f3b884696f3aaf2a8c
Mike Owen <mikeowen@llnl.gov>
2013-07-12 22:20:48 -0700

    Adding an experimental python binding to writing silo files.  Compiling, but not tested yet.

---
+++ src/PBGWraps/FunctionsModule.py
@@ -0,0 +1,58 @@
+from pybindgen import *
+
+import sys
+sys.path.append(".")
+from PBGutils import *
+
+#-------------------------------------------------------------------------------
+# Class to handle wrapping this module.
+#-------------------------------------------------------------------------------
+class Functions:
+
+    #---------------------------------------------------------------------------
+    # Add the types to the given module.
+    #---------------------------------------------------------------------------
+    def __init__(self, mod):
+        
+        # Includes
+        mod.add_include('"PolytopeTypes.hh"')
+
+        # Namespaces
+        polytope = mod.add_cpp_namespace("polytope")
+
+        self.objs = []
+
+        return
+    
+    #---------------------------------------------------------------------------
+    # Generate bindings.
+    #---------------------------------------------------------------------------
+    def generateBindings(self, mod):
+        polytope = mod.add_cpp_namespace("polytope")
+                       
+        for dim in (2, 3):
+            polytope.add_function("writeTessellation", 
+                                  None,
+                                  [constrefparam("polytope::Tessellation%id" % dim, "mesh"),
+                                   param("std::string", "filePrefix"),
+                                   param("PyObject*", "nodeFields", transfer_ownership=False),
+                                   param("PyObject*", "edgeFields", transfer_ownership=False),
+                                   param("PyObject*", "faceFields", transfer_ownership=False),
+                                   param("PyObject*", "cellFields", transfer_ownership=False),
+                                   param("int", "cycle", default_value = "0"),
+                                   param("double", "time", default_value = "0.0")],
+                                  template_parameters = [str(dim), "double"],
+                                  custom_name = "writeTessellation",
+                                  docstring = """writeTessellation -- output a silo file with optional fields
+arguments:
+  mesh       : the tessellation to write
+  filePrefix : prefix for the output file name
+  nodeFields : (optional, default=None) dictionary of (name -> vals) node centered values to put in file
+  edgeFields : (optional, default=None) dictionary of (name -> vals) edge centered values to put in file
+  faceFields : (optional, default=None) dictionary of (name -> vals) face centered values to put in file
+  cellFields : (optional, default=None) dictionary of (name -> vals) cell centered values to put in file
+  cycle      : (optional, default=0) cycle number
+  time       : (optional, default=0.0) time
+""")
+
+        return
--- src/PBGWraps/PolytopeTypes.hh
+++ src/PBGWraps/PolytopeTypes.hh
@@ -1,6 +1,7 @@
 #ifndef __PBGWRAPS_POLYTOPETYPES__
 #define __PBGWRAPS_POLYTOPETYPES__
 
+#include "Python.h"
 #include "polytope.hh"
 
 #include "PLC.hh"
@@ -10,6 +11,7 @@
 #include "BoostTessellator.hh"
 #include "TetgenTessellator.hh"
 #include "MeshEditor.hh"
+#include "SiloWriter.hh"
 
 #if HAVE_MPI
 #include "DistributedTessellator.hh"
@@ -184,6 +186,112 @@ void setsharedFaces( Tessellation<Dimension, RealType>& self,
                      std::vector<std::vector<unsigned> >* sharedFacesIn )
 { self.sharedFaces = *sharedFacesIn; }
 
+//------------------------------------------------------------------------------
+// Wrap writing silo files.
+//------------------------------------------------------------------------------
+// Start out with a few helper methods in the unnamed namespace.
+namespace {
+  template<typename RealType>
+  std::map<std::string, RealType*>
+  buildMapFromPyObject(PyObject* obj, 
+                       const unsigned expectedSize) {
+    std::map<std::string, RealType*> result;
+    if (obj == Py_None) return result;
+    if (not PyDict_Check(obj)) {
+      std::cerr << "ERROR : need to pass a dictionary of lists with string keys." << std::endl;
+      return result;
+    }
+    char* label;
+    PyObject *key, *item, *val;
+    Py_ssize_t i = 0, j;
+    while (PyDict_Next(obj, &i, &key, &item)) {
+      if (not PyString_Check(key)) {
+        std::cerr << "ERROR : keys need to be strings." << std::endl;
+        return result;
+      }
+      label = PyString_AsString(key);
+      result[std::string(label)] = new RealType[expectedSize];
+      if (not PyList_Check(item)) {
+        std::cerr << "ERROR : values need to be lists of floats." << std::endl;
+        return result;
+      }
+      if (PyList_Size(item) != expectedSize) {
+        std::cerr << "ERROR : list of values incorrect size : " << PyList_Size(item) << " != " << expectedSize << std::endl;
+        return result;
+      }
+      for (j = 0; j != expectedSize; ++j) {
+        val = PyList_GetItem(item, j);
+        if (not PyFloat_Check(val)) {
+          std::cerr << "ERROR : list of values should be convertable to floats." << std::endl;
+          return result;
+        }
+        result[std::string(label)][j] = PyFloat_AsDouble(val);
+      }
+    }
+    return result;
+  }
+
+  // Clean up memory.
+  template<typename RealType>
+  void
+  deleteMemory(std::map<std::string, RealType*>& stuff) {
+    for (typename std::map<std::string, RealType*>::iterator itr = stuff.begin();
+         itr != stuff.end();
+         ++itr) {
+      delete [] (itr->second);
+    }
+  }
+}
+
+template<int Dimension, typename RealType>
+inline
+void
+writeTessellation(const Tessellation<Dimension, RealType>& mesh,
+                  std::string filePrefix,
+                  PyObject* nodeFieldsDict,
+                  PyObject* edgeFieldsDict,
+                  PyObject* faceFieldsDict,
+                  PyObject* cellFieldsDict,
+                  int cycle,
+                  RealType time) {
+
+#if HAVE_SILO
+  // Extract the various optional fields.
+  std::map<std::string, RealType*> nodeFields = buildMapFromPyObject<RealType>(nodeFieldsDict, mesh.nodes.size()/Dimension);
+  std::map<std::string, RealType*> faceFields = buildMapFromPyObject<RealType>(faceFieldsDict, mesh.faces.size());
+  std::map<std::string, RealType*> cellFields = buildMapFromPyObject<RealType>(cellFieldsDict, mesh.cells.size());
+
+  // The edge fields don't currently work in 3D, so screen that case out.
+  std::map<std::string, RealType*> edgeFields;
+  if (Dimension == 3) {
+    if (edgeFieldsDict != Py_None) {
+      std::cerr << "WARNING : writing edge fields currently not supported in 3D.  Ignoring passed edgeFields set." << std::endl;
+    }
+  } else {
+    edgeFields = buildMapFromPyObject<RealType>(edgeFieldsDict, mesh.faces.size());
+  }
+
+  // Do the deed.
+  SiloWriter<Dimension, RealType>::write(mesh,
+                                         nodeFields,
+                                         edgeFields,
+                                         faceFields,
+                                         cellFields,
+                                         filePrefix,
+                                         cycle,
+                                         time);
+
+  // Clean up our memory.
+  deleteMemory<RealType>(nodeFields);
+  deleteMemory<RealType>(edgeFields);
+  deleteMemory<RealType>(faceFields);
+  deleteMemory<RealType>(cellFields);
+
+#else
+  std::cerr << "WARNING : apparently polytope was built without silo support, so cannot write silo files." << std::endl;
+#endif
+}
+  
 }
 
 #endif
