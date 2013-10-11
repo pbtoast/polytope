2e49f42a9f3bcb9c2d6b98a62d16a69a1e128af0
Mike Owen <mikeowen@llnl.gov>
2013-10-10 17:09:40 -0700

    Rewrote the 3D convex hull from scratch (this time I bothered to figure out the algorithm and implement it myself).  Seems to be working, but need more testing.

--- src/convexHull_3d.hh
+++ src/convexHull_3d.hh
@@ -1,6 +1,7 @@
 //----------------------------------------------------------------------------//
 // 3D implementation of the convex hull algorithm.
-// My own (possibly loose) interpretation of quick hull in 3D.
+// My own (possibly loose) interpretation of the incremental hull algorithm
+// in 3D (JMO).
 //----------------------------------------------------------------------------//
 #ifndef __polytope_convexHull_3d__
 #define __polytope_convexHull_3d__
@@ -138,62 +139,32 @@ struct TriangleFacet {
 
   unsigned inode, jnode, knode;          // Indices of the vertices of the triangle.
   PointType normal;                      // Normal to the surface.
-  std::vector<unsigned> remainingPoints; // Indices of points above this facet. 
-  const std::vector<PointType>* points;  // The full set of point positions (i, j, k) refer to.
 
-  TriangleFacet(): inode(0), jnode(0), knode(0), normal(), remainingPoints(), points(0) {}
+  TriangleFacet(): inode(0), jnode(0), knode(0), normal() {}
   TriangleFacet(const unsigned i, 
                 const unsigned j,
                 const unsigned k,
-                const std::vector<unsigned>& remainingPoints,
                 const std::vector<PointType>& points): inode(i), 
                                                        jnode(j),
                                                        knode(k),
-                                                       normal(),
-                                                       remainingPoints(remainingPoints),
-                                                       points(&points) {
-    computeNormal();
+                                                       normal() {
+    this->computeNormal(points);
   }
 
-  void computeNormal() {
-    const PointType ij = (*points)[jnode] - (*points)[inode];
-    const PointType ik = (*points)[knode] - (*points)[inode];
+  void computeNormal(const std::vector<PointType>& points) {
+    const PointType ij = points[jnode] - points[inode];
+    const PointType ik = points[knode] - points[inode];
     geometry::cross<3, RealType>(&ij.x, &ik.x, &normal.x);
     geometry::unitVector<3, RealType>(&normal.x);
   }
 
-  int compare(const PointType& point) const { 
-    const PointType ip = point - (*points)[inode];
+  int compare(const PointType& point, 
+              const std::vector<PointType>& points) const { 
+    const PointType ip = point - points[inode];
     const RealType test = geometry::dot<3, RealType>(&ip.x, &normal.x);
-    return (test < 0 ? -1 :
-            test > 0 ?  1 :
-            0);
-  }
-
-  void downselectRemainingPoints() {
-    std::vector<unsigned> result;
-    for (typename std::vector<unsigned>::const_iterator itr = remainingPoints.begin();
-         itr != remainingPoints.end();
-         ++itr) {
-      if (this->compare((*points)[*itr]) == 1) result.push_back(*itr);
-    }
-    remainingPoints = result;
-  }
-
-  unsigned highestRemainingPoint() const {
-    int result = -1;
-    RealType maxAltitude = 0.0;
-    for (typename std::vector<unsigned>::const_iterator itr = remainingPoints.begin();
-         itr != remainingPoints.end();
-         ++itr) {
-      const RealType altitude = pointPlaneDistance((*points)[*itr], (*points)[inode], normal);
-      if (altitude > maxAltitude) {
-        maxAltitude = altitude;
-        result = *itr;
-      }
-    }
-    POLY_ASSERT(result >= 0);
-    return unsigned(result);
+    return (std::abs(test) < 1.0e-8 ? 0 :
+            test < 0 ? -1 :
+            1);
   }
 
   void flip() { std::swap(inode, jnode); normal *= -1; }
@@ -203,27 +174,126 @@ struct TriangleFacet {
   // output operator
   friend std::ostream& operator<<(std::ostream& os, const TriangleFacet& facet) {
     os << "TriangleFacet(" << facet.inode << " " << facet.jnode << " " << facet.knode 
-       << ") : remaining points = (";
-    for (std::vector<unsigned>::const_iterator itr = facet.remainingPoints.begin();
-         itr != facet.remainingPoints.end();
-         ++itr) os << " " << *itr;
-    os << ")";
+       << "), normal = " << facet.normal;
     return os;
   }
 };
 
+//------------------------------------------------------------------------------
+// Reduce the given set of point indices to those exterior to the facets.
+//------------------------------------------------------------------------------
+template<typename RealType>
+void 
+exteriorPoints(std::vector<unsigned>& indices,
+               const std::vector<TriangleFacet<RealType> >& facets, 
+               const std::vector<Point3<RealType> >& points) {
+  typedef std::vector<TriangleFacet<RealType> > FacetSet;
+  typedef Point3<RealType> PointType;
+
+  std::vector<unsigned> remainingPoints;
+  for (std::vector<unsigned>::const_iterator iitr = indices.begin();
+       iitr != indices.end();
+       ++iitr) {
+    typename FacetSet::const_iterator fitr = facets.begin();
+    while (fitr != facets.end() and fitr->compare(points[*iitr], points) != 1) ++fitr;
+    if (fitr != facets.end()) remainingPoints.push_back(*iitr);
+  }
+  indices = remainingPoints;
 }
 
 //------------------------------------------------------------------------------
-// The 3D convex hull itself.  This is the one users should call -- it forwards
-// all work to the worker ConvexHull3d class.
+// Reduce the given set of point indices to those exterior to the facets.
+//------------------------------------------------------------------------------
+template<typename RealType>
+unsigned highestPoint(std::vector<unsigned>& indices,
+                      const std::vector<TriangleFacet<RealType> >& facets, 
+                      const std::vector<Point3<RealType> >& points) {
+  typedef std::vector<TriangleFacet<RealType> > FacetSet;
+  typedef Point3<RealType> PointType;
+  int result = -1;
+  RealType maxAltitude = 0.0;
+  for (typename std::vector<unsigned>::const_iterator itr = indices.begin();
+       itr != indices.end();
+       ++itr) {
+    for (typename FacetSet::const_iterator fitr = facets.begin();
+         fitr != facets.end();
+         ++fitr) {
+      const RealType altitude = pointPlaneDistance(points[*itr], points[fitr->inode], fitr->normal);
+      if (altitude > maxAltitude) {
+        maxAltitude = altitude;
+        result = *itr;
+      }
+    }
+  }
+  POLY_ASSERT(result >= 0);
+  return unsigned(result);
+}
+
+//------------------------------------------------------------------------------
+// Cull out the facets visible from the given point (points[apex]).
+// Also computes the remaining horizon edges.
+//------------------------------------------------------------------------------
+template<typename RealType>
+void cullVisibleFacets(std::vector<std::pair<int, int> >& horizonEdges,
+                       std::vector<TriangleFacet<RealType> >& facets, 
+                       const std::vector<Point3<RealType> >& points,
+                       const unsigned apex) {
+
+  typedef std::pair<int, int> EdgeHash;
+  typedef std::vector<TriangleFacet<RealType> > FacetSet;
+  typedef Point3<RealType> PointType;
+
+  FacetSet newFacets;
+  internal::CounterMap<EdgeHash> edgeCount;
+
+  // Walk the facets.
+  for (typename FacetSet::const_iterator fitr = facets.begin();
+       fitr != facets.end();
+       ++fitr) {
+
+    // Is this facet visible from the apex point?
+    if (fitr->compare(points[apex], points) == 1) {
+
+      // Yep, it needs to be removed.  Increment the use count for each
+      // of it's edges.
+      ++edgeCount[internal::hashEdge(fitr->inode, fitr->jnode)];
+      ++edgeCount[internal::hashEdge(fitr->jnode, fitr->knode)];
+      ++edgeCount[internal::hashEdge(fitr->knode, fitr->inode)];
+
+    } else {
+
+      // Nope, copy it to the new set.
+      newFacets.push_back(*fitr);
+
+    }
+  }
+  facets = newFacets;
+
+  // The horizon edges are the ones with a single use count.  All others should
+  // have been hit twice.
+  horizonEdges = std::vector<EdgeHash>();
+  for (internal::CounterMap<EdgeHash>::const_iterator eitr = edgeCount.begin();
+       eitr != edgeCount.end();
+       ++eitr) {
+    POLY_ASSERT(eitr->second == 1 or eitr->second == 2);
+    if (eitr->second == 1) horizonEdges.push_back(eitr->first);
+  }
+}
+
+} // anonymous namespace
+
+//------------------------------------------------------------------------------
+// The 3D convex hull method.
 //------------------------------------------------------------------------------
 template<typename RealType>
 PLC<3, RealType>
 convexHull_3d(const std::vector<RealType>& points,
               const RealType* low,
               const RealType& dx) {
+
   typedef Point3<RealType> PointType;
+  typedef std::vector<TriangleFacet<RealType> > FacetSet;
+  typedef std::pair<int, int> EdgeHash;
 
   // Pre-conditions.
   POLY_ASSERT(points.size() % 3 == 0);
@@ -260,7 +330,6 @@ convexHull_3d(const std::vector<RealType>& points,
                  boxz2 = geometry::dot<3, RealType>(&boxz.x, &boxz.x);
   POLY_ASSERT(boxx2 > 0.0 and boxy2 > 0.0 and boxz2 > 0.0);
   TriangleFacet<RealType> startingFacet;
-  startingFacet.points = &ps;
   if (boxx2 >= std::max(boxy2, boxz2)) {
     startingFacet.inode = ixmin;
     startingFacet.jnode = ixmax;
@@ -278,75 +347,73 @@ convexHull_3d(const std::vector<RealType>& points,
   PointType lineDirection = ps[startingFacet.jnode] - ps[startingFacet.inode];
   geometry::unitVector<3, RealType>(&lineDirection.x);
   startingFacet.knode = std::max_element(ps.begin(), ps.end(), CompareLineDistance<RealType>(ps[startingFacet.inode], lineDirection))->index;
-  startingFacet.computeNormal();
-  startingFacet.remainingPoints.resize(ps.size());
-  for (unsigned i = 0; i != ps.size(); ++i) startingFacet.remainingPoints[i] = i;
+  startingFacet.computeNormal(ps);
 
   // We have the triangular base, so pick the furthest point from this base as the apex of our
   // starting tetrahedron.  This point should also be on the hull.
   const unsigned apex = std::max_element(ps.begin(), ps.end(), CompareAbsPlaneDistance<RealType>(ps[startingFacet.inode], startingFacet.normal))->index;
 
   // If the apex point is below our starting facet, flip the facet.
-  if (startingFacet.compare(ps[apex]) == 1) startingFacet.flip();
+  if (startingFacet.compare(ps[apex], ps) == 1) startingFacet.flip();
 
-  // Create our working facets from the starting tetrahedron.
-  typedef typename std::vector<TriangleFacet<RealType> > FacetSet;
+  // Intialize our starting tetrahedron.
   FacetSet workingFacets;
   workingFacets.push_back(startingFacet);
-  workingFacets.push_back(TriangleFacet<RealType>(startingFacet.jnode, startingFacet.inode, apex, startingFacet.remainingPoints, ps));
-  workingFacets.push_back(TriangleFacet<RealType>(startingFacet.inode, startingFacet.knode, apex, startingFacet.remainingPoints, ps));
-  workingFacets.push_back(TriangleFacet<RealType>(startingFacet.knode, startingFacet.jnode, apex, startingFacet.remainingPoints, ps));
-  for (typename FacetSet::iterator facetItr = workingFacets.begin();
-       facetItr != workingFacets.end();
-       ++facetItr) {
-    facetItr->downselectRemainingPoints();
-  }
+  workingFacets.push_back(TriangleFacet<RealType>(startingFacet.jnode, startingFacet.inode, apex, ps));
+  workingFacets.push_back(TriangleFacet<RealType>(startingFacet.inode, startingFacet.knode, apex ,ps));
+  workingFacets.push_back(TriangleFacet<RealType>(startingFacet.knode, startingFacet.jnode, apex, ps));
+  // std::cerr << "Starting facets: " << std::endl
+  //           << "    " << workingFacets[0] << std::endl
+  //           << "    " << workingFacets[1] << std::endl
+  //           << "    " << workingFacets[2] << std::endl
+  //           << "    " << workingFacets[3] << std::endl;
 
-  // Iterate until all points are interior to our facet set.
-  bool done = false;
-  while (not done) {
-    done = true;
-    FacetSet newFacets;
+  // Find the set of points exterior to our starting tetrahedron.
+  std::vector<unsigned> remainingPoints(ps.size());
+  for (unsigned i = 0; i != ps.size(); ++i) remainingPoints[i] = i;
+  exteriorPoints(remainingPoints, workingFacets, ps);
 
-    std::cerr << "NEW PASS!" << std::endl;
-    for (typename FacetSet::iterator facetItr = workingFacets.begin();
-         facetItr != workingFacets.end();
-         ++facetItr) {
+  // Iterate until all points are interior to the facets.
+  while (!remainingPoints.empty()) {
 
-      // Is this facet done yet?
-      if (facetItr->remainingPoints.empty()) {
+    // Find the remaining point furthest from the hull.
+    const unsigned apex = highestPoint(remainingPoints, workingFacets, ps);
 
-        // Yep, copy it to the new set.
-        std::cerr << "      Final facet : " << (*facetItr) << std::endl;
-        newFacets.push_back(*facetItr);
+    // Remove the visible facets from apex, and compute the horizon edges.
+    std::vector<EdgeHash> horizonEdges;
+    cullVisibleFacets(horizonEdges, workingFacets, ps, apex);
+    POLY_ASSERT(horizonEdges.size() >= 3);
 
-      } else {
-
-        // Nope, we need to refine new facets from this one.
-        done = false;
-
-        // Find the point furthest above this facet.
-        const unsigned apex = facetItr->highestRemainingPoint();
-        std::cerr << "      Refining facet : " << (*facetItr) << " with new apex " << apex << std::endl;
-
-        // Add our new facets.
-        TriangleFacet<RealType> afacet(startingFacet.jnode, startingFacet.inode, apex, facetItr->remainingPoints, ps),
-                                bfacet(startingFacet.inode, startingFacet.knode, apex, facetItr->remainingPoints, ps),
-                                cfacet(startingFacet.knode, startingFacet.jnode, apex, facetItr->remainingPoints, ps);
-        afacet.downselectRemainingPoints();
-        bfacet.downselectRemainingPoints();
-        cfacet.downselectRemainingPoints();
-        std::cerr << "   new facet : " << afacet << std::endl
-                  << "   new facet : " << bfacet << std::endl
-                  << "   new facet : " << cfacet << std::endl;
-        newFacets.push_back(afacet);
-        newFacets.push_back(bfacet);
-        newFacets.push_back(cfacet);
-      }
+    // Build the new facets from the apex point and horizon edges.
+    // const PointType testPoint = (ps[workingFacets[0].inode] + ps[workingFacets[0].jnode] + ps[workingFacets[0].knode])/3.0;
+    PointType testPoint;
+    for (typename std::vector<EdgeHash>::const_iterator eitr = horizonEdges.begin();
+         eitr != horizonEdges.end();
+         ++eitr) {
+      testPoint += ps[eitr->first];
+      testPoint += ps[eitr->second];
+    }
+    testPoint /= 2*horizonEdges.size();
+    for (typename std::vector<EdgeHash>::const_iterator eitr = horizonEdges.begin();
+         eitr != horizonEdges.end();
+         ++eitr) {
+      workingFacets.push_back(TriangleFacet<RealType>(eitr->first, eitr->second, apex, ps));
+      if (workingFacets.back().compare(testPoint, ps) == 1) workingFacets.back().flip();
     }
 
-    // Assign the new set of Facets.
-    workingFacets = newFacets;
+    // Reduce to the new set of exterior points.
+    const unsigned oldsize = remainingPoints.size();
+    exteriorPoints(remainingPoints, workingFacets, ps);
+    POLY_ASSERT(remainingPoints.size() < oldsize);
+
+    // std::cerr << "New pass at facets: " << std::endl;
+    // for (unsigned i = 0; i != workingFacets.size(); ++i) {
+    //   std::cerr << " --> " << workingFacets[i] << std::endl;
+    // }
+    // std::cerr << "Remaining points : ";
+    // std::copy(remainingPoints.begin(), remainingPoints.end(), std::ostream_iterator<unsigned>(std::cerr, " "));
+    // std::cerr << std::endl;
+    // std::cerr << "  --> " << remainingPoints.size() << std::endl;
   }
 
   // Read out the data to the PLC and we're done.
