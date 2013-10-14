592c0b3d6bd7b2b5e1fa462f3fdfe3ab3cf027d4
Mike Owen <mikeowen@llnl.gov>
2013-10-14 10:58:06 -0700

    Starting to try generalizing to make int coordinates possible.

--- src/PLC_CSG.cc
+++ src/PLC_CSG.cc
@@ -9,6 +9,7 @@ namespace polytope {
     namespace CSG_internal {
       template<> float  Plane<float>::EPSILON  = 1.0e-8f;
       template<> double Plane<double>::EPSILON = 1.0e-10;
+      template<> int64_t Plane<int64_t>::EPSILON = 2;
     }
   }
 }
