ff98c5372af91572758bb2d77655314776230494
Mike Owen <mikeowen@llnl.gov>
2013-10-20 14:59:56 -0700

    Adding a unit test of our hashing/unhashing code, which has caught some subtle issues.

--- src/polytope_geometric_utilities.hh
+++ src/polytope_geometric_utilities.hh
@@ -94,8 +94,8 @@ template<typename RealType> struct Hasher<2, RealType> {
     // Quantize away.
     const RealType dx[2] = {std::max(RealType((xhigh[0] - xlow[0])/coordMax()), std::max(minTol, std::numeric_limits<RealType>::epsilon())),
                             std::max(RealType((xhigh[1] - xlow[1])/coordMax()), std::max(minTol, std::numeric_limits<RealType>::epsilon()))};
-    result += (uint64_t(std::min(coordMax(), uint64_t((pos[0] - xlow[0])/dx[0] + 0.5))) +
-               uint64_t(std::min(coordMax(), uint64_t((pos[1] - xlow[1])/dx[1] + 0.5)) << 31));
+    result += (uint64_t(std::min(coordMax(), uint64_t((pos[0] - xlow[0])/dx[0]))) +
+               uint64_t(std::min(coordMax(), uint64_t((pos[1] - xlow[1])/dx[1])) << 31));
     return result;
   }
 
@@ -127,8 +127,8 @@ template<typename RealType> struct Hasher<2, RealType> {
     // Extract the position (for the center of the cell).
     const RealType dx[2] = {std::max(RealType((xhigh[0] - xlow[0])/coordMax()), std::max(minTol, std::numeric_limits<RealType>::epsilon())),
                             std::max(RealType((xhigh[1] - xlow[1])/coordMax()), std::max(minTol, std::numeric_limits<RealType>::epsilon()))};
-    pos[0] = std::max(xlow[0], std::min(xhigh[0], RealType(xlow[0] + ( (hashedPosition & xmask())        + 0.5)*dx[0])));
-    pos[1] = std::max(xlow[1], std::min(xhigh[1], RealType(xlow[1] + (((hashedPosition & ymask()) >> 31) + 0.5)*dx[1])));
+    pos[0] = std::max(xlow[0], std::min(xhigh[0], RealType(xlow[0] + RealType(( (hashedPosition & xmask())        + 0.5)*dx[0]))));
+    pos[1] = std::max(xlow[1], std::min(xhigh[1], RealType(xlow[1] + RealType((((hashedPosition & ymask()) >> 31) + 0.5)*dx[1]))));
 
     // Post-conditions.
     POLY_ASSERT2(pos[0] >= xlow[0] and pos[0] <= xhigh[0] and
@@ -192,9 +192,9 @@ template<typename RealType> struct Hasher<3, RealType> {
     const RealType dx[3] = {std::max(RealType((xhigh[0] - xlow[0])/coordMax()), std::max(std::numeric_limits<RealType>::epsilon(), minTol)),
                             std::max(RealType((xhigh[1] - xlow[1])/coordMax()), std::max(std::numeric_limits<RealType>::epsilon(), minTol)),
                             std::max(RealType((xhigh[2] - xlow[2])/coordMax()), std::max(std::numeric_limits<RealType>::epsilon(), minTol))};
-    result += (uint64_t(std::min(coordMax(), uint64_t((pos[0] - xlow[0])/dx[0] + 0.5))) +
-               uint64_t(std::min(coordMax(), uint64_t((pos[1] - xlow[1])/dx[1] + 0.5)) << 21) +
-               uint64_t(std::min(coordMax(), uint64_t((pos[2] - xlow[2])/dx[2] + 0.5)) << 42));
+    result += (uint64_t(std::min(coordMax(), uint64_t((pos[0] - xlow[0])/dx[0]))) +
+               uint64_t(std::min(coordMax(), uint64_t((pos[1] - xlow[1])/dx[1])) << 21) +
+               uint64_t(std::min(coordMax(), uint64_t((pos[2] - xlow[2])/dx[2])) << 42));
     return result;
   }
 
@@ -230,9 +230,9 @@ template<typename RealType> struct Hasher<3, RealType> {
     const RealType dx[3] = {std::max(RealType((xhigh[0] - xlow[0])/coordMax()), std::max(minTol, std::numeric_limits<RealType>::epsilon())),
                             std::max(RealType((xhigh[1] - xlow[1])/coordMax()), std::max(minTol, std::numeric_limits<RealType>::epsilon())),
                             std::max(RealType((xhigh[2] - xlow[2])/coordMax()), std::max(minTol, std::numeric_limits<RealType>::epsilon()))};
-    pos[0] = std::max(xlow[0], std::min(xhigh[0], RealType(xlow[0] + ( (hashedPosition & xmask())        + 0.5)*dx[0])));
-    pos[1] = std::max(xlow[1], std::min(xhigh[1], RealType(xlow[1] + (((hashedPosition & ymask()) >> 21) + 0.5)*dx[1])));
-    pos[2] = std::max(xlow[2], std::min(xhigh[2], RealType(xlow[2] + (((hashedPosition & zmask()) >> 42) + 0.5)*dx[2])));
+    pos[0] = std::max(xlow[0], std::min(xhigh[0], RealType(xlow[0] + RealType(( (hashedPosition & xmask())        + 0.5)*dx[0]))));
+    pos[1] = std::max(xlow[1], std::min(xhigh[1], RealType(xlow[1] + RealType((((hashedPosition & ymask()) >> 21) + 0.5)*dx[1]))));
+    pos[2] = std::max(xlow[2], std::min(xhigh[2], RealType(xlow[2] + RealType((((hashedPosition & zmask()) >> 42) + 0.5)*dx[2]))));
 
     // Post-conditions.
     POLY_ASSERT2(pos[0] >= xlow[0] and pos[0] <= xhigh[0] and
---
+++ tests/test_hash.cc
@@ -0,0 +1,203 @@
+// Unit tests for our hashing algorithms.
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <cassert>
+#include <ctime>
+
+#include "polytope.hh"
+#include "polytope_internal.hh"
+#include "polytope_geometric_utilities.hh"
+#include "polytope_test_utilities.hh"
+
+#if HAVE_MPI
+#include "mpi.h"
+#endif
+
+using namespace std;
+using namespace polytope;
+
+namespace {
+
+//------------------------------------------------------------------------------
+// The common checking code for our various hashing tests.
+//------------------------------------------------------------------------------
+template<int Dimension, typename RealType>
+void hashSlingingSlasher(const RealType* xmin,
+                         const RealType* xmax,
+                         const RealType tol,
+                         const std::vector<RealType>& coords) {
+  POLY_CHECK(coords.size() % Dimension == 0);
+  typedef polytope::geometry::Hasher<Dimension, RealType> HasherType;
+  const unsigned n = coords.size() / Dimension;
+  std::map<uint64_t, std::vector<unsigned> > hash2ids;
+  RealType pos[2];
+  for (unsigned i = 0; i != n; ++i) {
+    for (unsigned j = 0; j != Dimension; ++j) {
+      POLY_CHECK(coords[Dimension*i + j] >= xmin[j] and coords[2*i + j] <= xmax[j]);
+    }
+    const uint64_t hashi = HasherType::hashPosition(&coords[Dimension*i], xmin, xmax, xmin, xmax, tol);
+    hash2ids[hashi].push_back(i);
+    for (std::vector<unsigned>::const_iterator itr = hash2ids[hashi].begin();
+         itr != hash2ids[hashi].end();
+         ++itr) {
+      POLY_CHECK((geometry::distance<Dimension, RealType>(&coords[Dimension*i], &coords[Dimension*(*itr)]) <= tol));
+    }
+    HasherType::unhashPosition(pos, xmin, xmax, xmin, xmax, hashi, tol);
+    POLY_CHECK2((geometry::distance<Dimension, RealType>(&coords[Dimension*i], pos) <= tol),
+                "Unhash position too far : " << (geometry::distance<Dimension, RealType>(&coords[Dimension*i], pos)));
+    // cerr << i << " (" << coords[2*i] << " " << coords[2*i+1] << ") --> " << hashi << " : (" 
+    //      << pos[0] << " " << pos[1] << ") --> " << HasherType::hashPosition(pos, xmin, xmax, xmin, xmax, tol) << endl;
+    POLY_CHECK(HasherType::hashPosition(pos, xmin, xmax, xmin, xmax, tol) == hashi);
+  }
+}
+
+}
+
+// -----------------------------------------------------------------------
+// main
+// -----------------------------------------------------------------------
+int main(int argc, char** argv) {
+
+#if HAVE_MPI
+  MPI_Init(&argc, &argv);
+#endif
+
+  const unsigned n = 1000;
+
+  //----------------------------------------------------------------------
+  // Hash a random set of points in 2D using a fine tolerance.
+  //----------------------------------------------------------------------
+  {
+    cout << "2D random hash test, doubles, fine tolerance." << endl;
+    const double xmin[2] = {-10.0, -10.0}, xmax[2] = {10.0, 10.0};
+    const double tol = 1.5e-5;
+    std::vector<double> coords(2*n);
+    for (unsigned i = 0; i != n; ++i) {
+      coords[2*i  ] = xmin[0] + polytope::random01()*(xmax[0] - xmin[0]);
+      coords[2*i+1] = xmin[1] + polytope::random01()*(xmax[1] - xmin[1]);
+    }
+    hashSlingingSlasher<2, double>(xmin, xmax, tol, coords);
+  }
+
+  //----------------------------------------------------------------------
+  // Hash a random set of points in 2D using a coarse tolerance.
+  //----------------------------------------------------------------------
+  {
+    cout << "2D random hash test, doubles, coarse tolerance." << endl;
+    const double xmin[2] = {-10.0, -10.0}, xmax[2] = {10.0, 10.0};
+    const double tol = 1.5e-1;
+    std::vector<double> coords(2*n);
+    for (unsigned i = 0; i != n; ++i) {
+      coords[2*i  ] = xmin[0] + polytope::random01()*(xmax[0] - xmin[0]);
+      coords[2*i+1] = xmin[1] + polytope::random01()*(xmax[1] - xmin[1]);
+    }
+    hashSlingingSlasher<2, double>(xmin, xmax, tol, coords);
+  }
+
+  //----------------------------------------------------------------------
+  // Hash a random set of points in 2D using a fine tolerance (ints).
+  //----------------------------------------------------------------------
+  {
+    cout << "2D random hash test, int64_t, fine tolerance." << endl;
+    const int64_t ixmax = (1 << 16);
+    const int64_t xmin[2] = {-ixmax, -ixmax}, xmax[2] = {ixmax, ixmax};
+    const int64_t tol = 1;
+    std::vector<int64_t> coords(2*n);
+    for (unsigned i = 0; i != n; ++i) {
+      coords[2*i  ] = xmin[0] + int64_t(polytope::random01()*(xmax[0] - xmin[0]));
+      coords[2*i+1] = xmin[1] + int64_t(polytope::random01()*(xmax[1] - xmin[1]));
+    }
+    hashSlingingSlasher<2, int64_t>(xmin, xmax, tol, coords);
+  }
+
+  //----------------------------------------------------------------------
+  // Hash a random set of points in 2D using a coarse tolerance (ints).
+  //----------------------------------------------------------------------
+  {
+    cout << "2D random hash test, int64_t, coarse tolerance." << endl;
+    const int64_t ixmax = (1 << 16);
+    const int64_t xmin[2] = {-ixmax, -ixmax}, xmax[2] = {ixmax, ixmax};
+    const int64_t tol = 32;
+    std::vector<int64_t> coords(2*n);
+    for (unsigned i = 0; i != n; ++i) {
+      coords[2*i  ] = xmin[0] + int64_t(polytope::random01()*(xmax[0] - xmin[0]));
+      coords[2*i+1] = xmin[1] + int64_t(polytope::random01()*(xmax[1] - xmin[1]));
+    }
+    hashSlingingSlasher<2, int64_t>(xmin, xmax, tol, coords);
+  }
+
+  //----------------------------------------------------------------------
+  // Hash a random set of points in 3D using a fine tolerance.
+  //----------------------------------------------------------------------
+  {
+    cout << "3D random hash test, doubles, fine tolerance." << endl;
+    const double xmin[3] = {-10.0, -10.0, -10.0}, xmax[3] = {10.0, 10.0, 10.0};
+    const double tol = 1.5e-5;
+    std::vector<double> coords(3*n);
+    for (unsigned i = 0; i != n; ++i) {
+      coords[3*i  ] = xmin[0] + polytope::random01()*(xmax[0] - xmin[0]);
+      coords[3*i+1] = xmin[1] + polytope::random01()*(xmax[1] - xmin[1]);
+      coords[3*i+2] = xmin[2] + polytope::random01()*(xmax[2] - xmin[2]);
+    }
+    hashSlingingSlasher<3, double>(xmin, xmax, tol, coords);
+  }
+
+  //----------------------------------------------------------------------
+  // Hash a random set of points in 3D using a coarse tolerance.
+  //----------------------------------------------------------------------
+  {
+    cout << "3D random hash test, doubles, coarse tolerance." << endl;
+    const double xmin[3] = {-10.0, -10.0, -10.0}, xmax[3] = {10.0, 10.0, 10.0};
+    const double tol = 1.5e-1;
+    std::vector<double> coords(3*n);
+    for (unsigned i = 0; i != n; ++i) {
+      coords[3*i  ] = xmin[0] + polytope::random01()*(xmax[0] - xmin[0]);
+      coords[3*i+1] = xmin[1] + polytope::random01()*(xmax[1] - xmin[1]);
+      coords[3*i+2] = xmin[2] + polytope::random01()*(xmax[2] - xmin[2]);
+    }
+    hashSlingingSlasher<3, double>(xmin, xmax, tol, coords);
+  }
+
+  //----------------------------------------------------------------------
+  // Hash a random set of points in 3D using a fine tolerance (ints).
+  //----------------------------------------------------------------------
+  {
+    cout << "3D random hash test, int64_t, fine tolerance." << endl;
+    const int64_t ixmax = (1 << 16);
+    const int64_t xmin[3] = {-ixmax, -ixmax, -ixmax}, xmax[3] = {ixmax, ixmax, ixmax};
+    const int64_t tol = 1;
+    std::vector<int64_t> coords(3*n);
+    for (unsigned i = 0; i != n; ++i) {
+      coords[3*i  ] = xmin[0] + int64_t(polytope::random01()*(xmax[0] - xmin[0]));
+      coords[3*i+1] = xmin[1] + int64_t(polytope::random01()*(xmax[1] - xmin[1]));
+      coords[3*i+2] = xmin[2] + int64_t(polytope::random01()*(xmax[2] - xmin[2]));
+    }
+    hashSlingingSlasher<3, int64_t>(xmin, xmax, tol, coords);
+  }
+
+  //----------------------------------------------------------------------
+  // Hash a random set of points in 3D using a coarse tolerance (ints).
+  //----------------------------------------------------------------------
+  {
+    cout << "3D random hash test, int64_t, coarse tolerance." << endl;
+    const int64_t ixmax = (1 << 16);
+    const int64_t xmin[3] = {-ixmax, -ixmax, -ixmax}, xmax[3] = {ixmax, ixmax, ixmax};
+    const int64_t tol = 33;
+    std::vector<int64_t> coords(3*n);
+    for (unsigned i = 0; i != n; ++i) {
+      coords[3*i  ] = xmin[0] + int64_t(polytope::random01()*(xmax[0] - xmin[0]));
+      coords[3*i+1] = xmin[1] + int64_t(polytope::random01()*(xmax[1] - xmin[1]));
+      coords[3*i+2] = xmin[2] + int64_t(polytope::random01()*(xmax[2] - xmin[2]));
+    }
+    hashSlingingSlasher<3, int64_t>(xmin, xmax, tol, coords);
+  }
+
+  cout << "PASS" << endl;
+
+#if HAVE_MPI
+   MPI_Finalize();
+#endif
+  return 0;
+}
