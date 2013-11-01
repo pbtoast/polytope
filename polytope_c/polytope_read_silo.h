4c609cca81447df7a83ab68120d6992606f1d3b6
Jeffrey Johnson <jeffjohnson@pobox.com>
2013-11-01 14:41:26 -0700

    Added support for reading tags to SiloReader (and C interface).

--- polytope_c/polytope_read_silo.h
+++ polytope_c/polytope_read_silo.h
@@ -31,6 +31,35 @@ void polytope_read_silo(polytope_tessellation_t* mesh,
                         int num_files,
                         int mpi_tag);
 
+//! This version allows tags to be read in addition to the mesh and field data.
+void polytope_read_silo_with_tags(polytope_tessellation_t* mesh, 
+                                  int* num_fields,
+                                  char*** field_names,
+                                  polytope_real_t*** fields,
+                                  int* num_node_tags,
+                                  char*** node_tag_names,
+                                  int** node_tag_sizes,
+                                  int*** node_tags,
+                                  int* num_edge_tags,
+                                  char*** edge_tag_names,
+                                  int** edge_tag_sizes,
+                                  int*** edge_tags,
+                                  int* num_face_tags,
+                                  char*** face_tag_names,
+                                  int** face_tag_sizes,
+                                  int*** face_tags,
+                                  int* num_cell_tags,
+                                  char*** cell_tag_names,
+                                  int** cell_tag_sizes,
+                                  int*** cell_tags,
+                                  const char* file_prefix,
+                                  const char* directory,
+                                  int cycle,
+                                  polytope_real_t time,
+                                  MPI_Comm comm,
+                                  int num_files,
+                                  int mpi_tag);
+
 #ifdef __cplusplus
 }
 #endif
