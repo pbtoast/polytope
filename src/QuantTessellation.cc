7f13611eabb5625aeac2ee86388e67ab2659273e
Mike Owen <mikeowen@llnl.gov>
2014-06-25 15:17:17 -0700

    Moving around how we declare specializations and such in QuantTessellation.

--- src/QuantTessellation.cc
+++ src/QuantTessellation.cc
@@ -5,9 +5,6 @@
 
 namespace polytope {
 namespace {
-}
-
-namespace internal {
 
 //----------------------------------------------------------------------------
 // Clip a completed QuantTessellation to the inner bounding box.  
@@ -21,44 +18,49 @@ namespace internal {
 template<typename RealType>
 void
 QuantTessellation<2, RealType>::
-clipToInnerBoundingBox() {
+clipToInnerBoundingBoxImpl(QuantTesssellation<2, RealType>& qmesh) {
+
+  typedef QuantTessellation<2, RealType> QtessType;
+  typedef QtessType::PointHash PointHash;
+  typedef QtessType::IntPoint IntPoint;
+  typedef QtessType::EdgeHash EdgeHash;
 
   // Figure out the four quantized corners of the inner box.
-  const PointHash qlow_inner = hashPosition(low_inner),
-                 qhigh_inner = hashPosition(high_inner);
-  POLY_ASSERT(!(qlow_inner & geometry::Hasher<2, RealType>::outerFlag()));
-  POLY_ASSERT(!(qhigh_inner & geometry::Hasher<2, RealType>::outerFlag()));
-  const CoordHash ixmin = geometry::Hasher<2, RealType>::qxval(qlow_inner),
-                  ixmax = geometry::Hasher<2, RealType>::qxval(qhigh_inner),
-                  iymin = geometry::Hasher<2, RealType>::qyval(qlow_inner),
-                  iymin = geometry::Hasher<2, RealType>::qyval(qhigh_inner);
+  const PointHash qlow_inner = hashPosition(qmesh.low_inner),
+                 qhigh_inner = hashPosition(qmesh.high_inner);
+  POLY_ASSERT(!(qmesh.qlow_inner  & geometry::Hasher<2, RealType>::outerFlag()));
+  POLY_ASSERT(!(qmesh.qhigh_inner & geometry::Hasher<2, RealType>::outerFlag()));
+  const CoordHash ixmin = geometry::Hasher<2, RealType>::qxval(qmesh.qlow_inner),
+                  ixmax = geometry::Hasher<2, RealType>::qxval(qmesh.qhigh_inner),
+                  iymin = geometry::Hasher<2, RealType>::qyval(qmesh.qlow_inner),
+                  iymin = geometry::Hasher<2, RealType>::qyval(qmesh.qhigh_inner);
   const IntPoint ibox0 = IntPoint(ixmin, iymin),
                  ibox1 = IntPoint(ixmax, iymin),
                  ibox2 = IntPoint(ixmax, iymax),
                  ibox3 = IntPoint(ixmin, iymax);
 
   // Flag all the infNodes for fast checking.
-  std::vector<int> infNodeFlags(points.size(), 0);
-  for (std::vector<unsigned>::iterator infItr = infNodes.begin();
-       infItr != infNodes.end();
+  std::vector<int> infNodeFlags(qmesh.points.size(), 0);
+  for (std::vector<unsigned>::iterator infItr = qmesh.infNodes.begin();
+       infItr != qmesh.infNodes.end();
        ++infItr) infNodeFlags[*infItr] = 1;
 
   // Clip the infNodes to the inner bounding surface.
-  const std::vector<std::vector<unsigned> > nodes2edges = this->nodeEdges();
+  const std::vector<std::vector<unsigned> > nodes2edges = qmesh.nodeEdges();
   std::vector<IntPoint> newPoints;
   std::vector<EdgeHash> newEdges;
-  const RealPoint c1 = low_inner, 
-                  c2 = RealPoint(high_inner.x, low_inner.y),
-                  c3 = high_inner.x,
-                  c4 = RealPoint(low_inner.x, high_inner.y);
-  for (std::vector<unsigned>::iterator infItr = infNodes.begin();
-       infItr != infNodes.end();
+  const RealPoint c1 = qmesh.low_inner, 
+                  c2 = RealPoint(qmesh.high_inner.x, qmesh.low_inner.y),
+                  c3 = qmesh.high_inner.x,
+                  c4 = RealPoint(qmesh.low_inner.x, qmesh.high_inner.y);
+  for (std::vector<unsigned>::iterator infItr = qmesh.infNodes.begin();
+       infItr != qmesh.infNodes.end();
        ++infItr) {
     bool newNode = false;
     const unsigned i = *infItr;
-    POLY_ASSERT(i < nodes2edges.size());
-    for (std::vector<unsigned>::const_iterator eItr = nodes2edges[i].begin();
-         eItr != nodes2edges[i].end();
+    POLY_ASSERT(i < qmesh.nodes2edges.size());
+    for (std::vector<unsigned>::const_iterator eItr = qmesh.nodes2edges[i].begin();
+         eItr != qmesh.nodes2edges[i].end();
          ++eItr) {
       const unsigned ei = *eItr;
       POLY_ASSERT(edges[ei].first == i or edges[ei].second == i);
@@ -71,6 +73,59 @@ clipToInnerBoundingBox() {
       }
     }
   }
+}
+
+//------------------------------------------------------------------------------
+// Hash an IntPoint.
+// Note currently these methods should only be used for a single level 
+// QuantTessellation -- no outer box!
+//------------------------------------------------------------------------------
+// 2D
+template<typename RealType>
+typename QuantTessellation<2, RealType>::PointHash
+
+hashIntPoint(const QuantTessellation<2, RealType>& qmesh,
+             const typename QuantTessellation<2, RealType>::IntPoint& p) {
+  POLY_ASSERT(qmesh.low_innner == qmesh.low_outer and 
+              qmesh.high_inner == qmesh.high_outer);
+  return geometry::Hasher<2, RealType>::hash(p.x, p.y);
+}
+
+// 3D
+template<typename RealType>
+typename QuantTessellation<3, RealType>::PointHash
+hashIntPoint(const QuantTessellation<3, RealType>& qmesh,
+             const typename QuantTessellation<3, RealType>::IntPoint& p) {
+  POLY_ASSERT(qmesh.low_innner == qmesh.low_outer and
+              qmesh.high_inner == qmesh.high_outer);
+  return geometry::Hasher<3, RealType>::hash(p.x, p.y, p.z);
+}
+
+}
+
+namespace internal {
+
+//----------------------------------------------------------------------------
+// Clip the the QuantTessellation to the inner bounding box.
+// We export this work to specialized 2D and 3D implementations.
+//----------------------------------------------------------------------------
+template<int Dimension, typename RealType>
+void
+QuantTessellation<Dimension, RealType>::
+clipToInnerBoundingBox() {
+  clipToInnerBoundingBoxImpl(*this);
+}
+
+//------------------------------------------------------------------------------
+// Hash an IntPoint.
+//------------------------------------------------------------------------------
+// 2D
+template<typename RealType>
+inline
+typename QuantTessellation<2, RealType>::PointHash
+QuantTessellation<2, RealType>::
+hashIntPoint(const typename QuantTessellation<2, RealType>::IntPoint& p) {
+  return hashIntPointImpl(*this, p);
 }
 
 }
