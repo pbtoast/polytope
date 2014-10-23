1f24e5cb00ed352bb43c6d185652d5a467b67575
David Starinshak <starinshak1@llnl.gov>
2014-10-23 16:44:49 -0700

    Removing a bogus assert

--- tests/test_Area.cc
+++ tests/test_Area.cc
@@ -59,11 +59,6 @@ double testBoundary(Boundary2D<double>& boundary,
     cout << "              Area  = " << area << endl;
     cout << "              Error = " << boundary.mArea - area << endl;
     cout << "   Fractional error = " << fracerr << endl;
-
-    outputMesh(mesh, "Blago", n);
-    
-    POLY_CHECK(false);
-
     mesh.clear();   
   }
   return result;
