5744ef77f35f07291263733419b8f78f48efafcf
David Starinshak <starinshak1@llnl.gov>
2015-04-03 16:14:42 -0700

    Added new tessellateNormalized routines to the base Tessellator class. These normalize the input points and geometry by a single min/max range across all dimensions. The idea here is to normalize the input data while maintaining the original aspect ratios. This is not a default option (yet?) but is accessible in both the C++ and Python interfaces. Initial testing indicates the clipping procedure is more robust when the cloud of input points is expanding in size.

--- src/TessellatorInline.hh
+++ src/TessellatorInline.hh
@@ -33,6 +33,80 @@ boundingBox(std::vector<RealType>& points) const
 }
 
 //------------------------------------------------------------------------------
+//! This helper method returns a normalized collection of points
+//! given minimal and maximal values across all dimensions. This maintains
+//! aspect ratios during the normalization
+//------------------------------------------------------------------------------
+template<typename RealType>
+inline
+std::vector<RealType>
+normalizePointsWithSameAspectRatio(const std::vector<RealType>& points,
+                                   const RealType xmin,
+                                   const RealType xmax)
+{
+   POLY_ASSERT(xmin < xmax);
+   const RealType boxInv = 1.0/std::max(1.0e-30, xmax - xmin);
+   const unsigned n = points.size();
+   std::vector<RealType> result(n);
+   for (unsigned i = 0; i < n; ++i) {
+      result[i] = (points[i] - xmin)*boxInv;
+      POLY_ASSERT(result[i] >= 0.0 and result[i] <= 1.0);
+   }
+   return result;
+}
+
+//------------------------------------------------------------------------------
+//! This helper method scales a collection of normalized points by a common
+//! value range to maintain aspect ratios.
+//------------------------------------------------------------------------------
+template<typename RealType>
+inline
+void
+denormalizePointsWithSameAspectRatio(std::vector<RealType>& normalizedPoints,
+                                     const RealType xmin,
+                                     const RealType xmax)
+{
+   POLY_ASSERT(xmin < xmax);
+   const RealType L = std::max(1.0e-30, xmax - xmin);
+   const unsigned n = normalizedPoints.size();
+   for (unsigned i = 0; i < n; ++i) {
+      POLY_ASSERT(normalizedPoints[i] >= 0.0 and normalizedPoints[i] <= 1.0);
+      normalizedPoints[i] = xmin + L*normalizedPoints[i];
+      // POLY_ASSERT2(normalizedPoints[i] >= xmin and normalizedPoints[i] <= xmax,
+      //              xmin << " " << normalizedPoints[i] << " " << xmax);
+   }
+}
+
+//------------------------------------------------------------------------------
+//! Return a normalized set of coordinates, also returning the bounding low/high points.
+//------------------------------------------------------------------------------
+template<int Dimension, typename RealType>
+inline
+void
+computeCommonBounds(const RealType* points,
+                    const int Npoints,
+                    const double safetyFactor,
+                    RealType& minVal,
+                    RealType& maxVal)
+{
+  minVal =  std::numeric_limits<RealType>::max();
+  maxVal = -std::numeric_limits<RealType>::max();       
+  RealType low[Dimension], high[Dimension];
+  geometry::computeBoundingBox<Dimension, RealType>(points, Npoints, true, low, high);
+  for (unsigned j = 0; j != Dimension; ++j) {
+    POLY_ASSERT(low[j] <= high[j]);
+    minVal = std::min(minVal, low[j] );
+    maxVal = std::max(maxVal, high[j]);
+    POLY_ASSERT(minVal < maxVal);
+  }
+  // Add a small safety factor.
+  const double dx = safetyFactor*(maxVal - minVal);
+  minVal -= dx;
+  maxVal += dx;
+}
+
+
+//------------------------------------------------------------------------------
 //! Return a normalized set of coordinates, also returning the bounding low/high points.
 //------------------------------------------------------------------------------
 template<int Dimension, typename RealType>
@@ -152,5 +226,111 @@ tessellateDegenerate(const std::vector<RealType>& points,
   this->tessellate(uniquePoints, PLCpoints, geometry, mesh);
   return result;
 }
+
+
+
+//------------------------------------------------------------------------------
+// Do a tessellation by first normalizing the input points.
+// Unbounded.
+//------------------------------------------------------------------------------
+template<int Dimension, typename RealType>
+inline
+void
+Tessellator<Dimension, RealType>::
+tessellateNormalized(const std::vector<RealType>& points,
+                     Tessellation<Dimension, RealType>& mesh) const {
+
+  // Compute the bounding box.
+  RealType xmin, xmax;
+  computeCommonBounds<Dimension, RealType>(&points[0], points.size(), 0.0, xmin, xmax);
+
+  // Normalize the input points
+  std::vector<RealType> normPoints = 
+     normalizePointsWithSameAspectRatio<RealType>(points, xmin, xmax);
+
+  // Tessellate the normalized points
+  this->tessellate(normPoints, mesh);
+
+  // Denormalize the mesh nodes
+  denormalizePointsWithSameAspectRatio<RealType>(mesh.nodes, xmin, xmax);
+}
+
+
+//------------------------------------------------------------------------------
+// Do a tessellation by first normalizing the input points.
+// Bounding box.
+//------------------------------------------------------------------------------
+template<int Dimension, typename RealType>
+inline
+void
+Tessellator<Dimension, RealType>::
+tessellateNormalized(const std::vector<RealType>& points,
+                     RealType* low,
+                     RealType* high,
+                     Tessellation<Dimension, RealType>& mesh) const {
+
+  // Compute the common bounds
+  RealType xmin =  std::numeric_limits<RealType>::max();
+  RealType xmax = -std::numeric_limits<RealType>::max();      
+  for (unsigned j = 0; j < Dimension; ++j) {
+    POLY_ASSERT(low[j] <= high[j]);
+    xmin = std::min(xmin, low[j] );
+    xmax = std::max(xmax, high[j]);
+  }
+  POLY_ASSERT(xmin < xmax);
+
+  // Normalize the input points
+  std::vector<RealType> normPoints = 
+     normalizePointsWithSameAspectRatio<RealType>(points, xmin, xmax);
+  
+  // Normalize the bounds
+  RealType normLow[Dimension], normHigh[Dimension];
+  for (unsigned j = 0; j < Dimension; ++j) {
+    normLow[j]  = (low[j]  - xmin) / (std::max(1.0e-30, xmax - xmin));
+    normHigh[j] = (high[j] - xmin) / (std::max(1.0e-30, xmax - xmin));
+  }
+
+  // Tessellate the normalized points
+  this->tessellate(normPoints, normLow, normHigh, mesh);
+
+  // Denormalize the mesh nodes
+  denormalizePointsWithSameAspectRatio<RealType>(mesh.nodes, xmin, xmax);
+}
+
+//------------------------------------------------------------------------------
+// Do a tessellation by first normalizing the input points.
+// PLC.
+//------------------------------------------------------------------------------
+template<int Dimension, typename RealType>
+inline
+void
+Tessellator<Dimension, RealType>::
+tessellateNormalized(const std::vector<RealType>& points,
+                     const std::vector<RealType>& PLCpoints,
+                     const PLC<Dimension, RealType>& geometry,
+                     Tessellation<Dimension, RealType>& mesh) const {
+
+  // Compute the common bounds
+  RealType xmin1, xmax1, xmin2, xmax2;
+  computeCommonBounds<Dimension, RealType>(&points[0],    points.size(),    0.0, xmin1, xmax1);
+  computeCommonBounds<Dimension, RealType>(&PLCpoints[0], PLCpoints.size(), 0.0, xmin2, xmax2);
+  const RealType xmin = std::min(xmin1, xmin2);
+  const RealType xmax = std::max(xmax1, xmax2);
+
+  // Normalize the input points
+  std::vector<RealType> normPoints = 
+     normalizePointsWithSameAspectRatio<RealType>(points, xmin, xmax);
+
+  // Normalize the PLC points
+  std::vector<RealType> normPLCPoints = 
+     normalizePointsWithSameAspectRatio<RealType>(PLCpoints, xmin, xmax);
+  
+  // Tessellate the normalized points
+  this->tessellate(normPoints, normPLCPoints, geometry, mesh);
+
+  // Denormalize the mesh nodes
+  denormalizePointsWithSameAspectRatio<RealType>(mesh.nodes, xmin, xmax);
+}
+
 
 }
