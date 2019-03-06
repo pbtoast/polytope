a946404321c26c02dc21acc2648b322eac14b9a0
Mike Owen <mikeowen@llnl.gov>
2019-03-06 14:52:52 -0800

    Added all the normal polytope function bindings.

--- src/PYB11/polytopeMOD.py
+++ src/PYB11/polytopeMOD.py
@@ -32,6 +32,7 @@ writePLCtoOFF2d = PYB11TemplateFunction(writePLCtoOFF, pyname="writePLCtoOFF", t
 writePLCtoOFF3d = PYB11TemplateFunction(writePLCtoOFF, pyname="writePLCtoOFF", template_parameters={"RealType": "double",
                                                                                                     "Dimension": "3"})
 
+#...............................................................................
 @PYB11template("int Dimension", "RealType")
 @PYB11implementation("""[](const Tessellation<%(Dimension)s, %(RealType)s>& mesh,
                            std::string filePrefix,
@@ -41,16 +42,20 @@ writePLCtoOFF3d = PYB11TemplateFunction(writePLCtoOFF, pyname="writePLCtoOFF", t
                            py::dict cellFieldsDict,
                            int cycle,
                            %(RealType)s time) {
+#ifdef HAVE_SILO
                                std::map<std::string, %(RealType)s*> nodeFields, edgeFields, faceFields, cellFields;
-                               std::vector<std::vector<%(RealType)s>> nodeVals, edgeVals, faceVals, cellVals;
-                               for (const auto item: nodeFieldsDict) {
-                                 nodeVals.push_back(std::vector<double>());
-                                 const auto key = item.first.cast<std::string>();
-                                 py::list vals = item.second.cast<py::list>();
-                                 for (const auto vali: vals) nodeVals.back().push_back(vali.cast<double>());
-                                 nodeFields[key] = &nodeVals.back().front();
-                               }
+                               auto nodeVals = pybind11_helpers::copyDictToMap<std::string, double>(nodeFieldsDict);
+                               auto edgeVals = pybind11_helpers::copyDictToMap<std::string, double>(edgeFieldsDict);
+                               auto faceVals = pybind11_helpers::copyDictToMap<std::string, double>(faceFieldsDict);
+                               auto cellVals = pybind11_helpers::copyDictToMap<std::string, double>(cellFieldsDict);
+                               for (auto& kv: nodeVals) nodeFields[kv.first] = &(kv.second.front());
+                               for (auto& kv: edgeVals) edgeFields[kv.first] = &(kv.second.front());
+                               for (auto& kv: faceVals) faceFields[kv.first] = &(kv.second.front());
+                               for (auto& kv: cellVals) cellFields[kv.first] = &(kv.second.front());
                                SiloWriter<%(Dimension)s, %(RealType)s>::write(mesh, nodeFields, edgeFields, faceFields, cellFields, filePrefix, cycle, time);
+#else
+                               throw std::runtime_error("Polytope built without SILO support");
+#endif
                            }""")
 def writeTessellation(mesh = "const Tessellation<%(Dimension)s, %(RealType)s>&",
                       filePrefix = "std::string",
@@ -64,3 +69,91 @@ def writeTessellation(mesh = "const Tessellation<%(Dimension)s, %(RealType)s>&",
     return "void"
 
 writeTessellation2d = PYB11TemplateFunction(writeTessellation, template_parameters=("2", "double"))
+writeTessellation3d = PYB11TemplateFunction(writeTessellation, template_parameters=("3", "double"))
+
+#...............................................................................
+@PYB11template("int Dimension", "RealType")
+@PYB11implementation("""[](const Tessellation<%(Dimension)s, %(RealType)s>& mesh,
+                           std::string filePrefix,
+                           std::string directory,
+                           py::dict nodeFieldsDict,
+                           py::dict edgeFieldsDict,
+                           py::dict faceFieldsDict,
+                           py::dict cellFieldsDict,
+                           int cycle,
+                           %(RealType)s time) {
+#ifdef HAVE_SILO
+                               std::map<std::string, %(RealType)s*> nodeFields, edgeFields, faceFields, cellFields;
+                               auto nodeVals = pybind11_helpers::copyDictToMap<std::string, double>(nodeFieldsDict);
+                               auto edgeVals = pybind11_helpers::copyDictToMap<std::string, double>(edgeFieldsDict);
+                               auto faceVals = pybind11_helpers::copyDictToMap<std::string, double>(faceFieldsDict);
+                               auto cellVals = pybind11_helpers::copyDictToMap<std::string, double>(cellFieldsDict);
+                               for (auto& kv: nodeVals) nodeFields[kv.first] = &(kv.second.front());
+                               for (auto& kv: edgeVals) edgeFields[kv.first] = &(kv.second.front());
+                               for (auto& kv: faceVals) faceFields[kv.first] = &(kv.second.front());
+                               for (auto& kv: cellVals) cellFields[kv.first] = &(kv.second.front());
+                               SiloWriter<%(Dimension)s, %(RealType)s>::write(mesh, nodeFields, edgeFields, faceFields, cellFields, filePrefix, directory, cycle, time);
+#else
+                               throw std::runtime_error("Polytope built without SILO support");
+#endif
+                           }""")
+def writeTessellationDir(mesh = "const Tessellation<%(Dimension)s, %(RealType)s>&",
+                         filePrefix = "std::string",
+                         directory = "std::string",
+                         nodeFieldsDict = "py::dict",
+                         edgeFieldsDict = "py::dict",
+                         faceFieldsDict = "py::dict",
+                         cellFieldsDict = "py::dict",
+                         cycle = "int",
+                         time = "%(RealType)s"):
+    "Write a tessellation (and arbitrary fields) to a silo file.  This version supports specifying the output directory."
+    return "void"
+
+writeTessellationDir2d = PYB11TemplateFunction(writeTessellationDir, template_parameters=("2", "double"), pyname="writeTessellation2d")
+writeTessellationDir3d = PYB11TemplateFunction(writeTessellationDir, template_parameters=("3", "double"), pyname="writeTessellation3d")
+
+#...............................................................................
+@PYB11template("RealType")
+@PYB11implementation("""[](py::list& points,
+                           py::tuple& low,
+                           %(RealType)s& dx) {
+                               const auto coords = pybind11_helpers::copyCoords<2, %(RealType)s>(points);
+                               std::vector<%(RealType)s> vlow = {low[0].cast<%(RealType)s>(), low[1].cast<%(RealType)s>()};
+                               auto result = convexHull_2d(coords, &vlow.front(), dx);
+                               return result;
+                           }""")
+def convexHull2d(points = "py::list",
+                 low = "py::tuple",
+                 dx = "%(RealType)s&"):
+    """Return the convex hull of the points in a list of tuple positions of the form
+  points = [(x,y), (x,y), ...]
+  low: a tuple of coordinates (x,y) which should be less than any of the coordinates in "points"
+  dx:  the precision for distinguishing between coordinate values
+"""
+    return "PLC<2, %(RealType)s>"
+
+constructConvexHull2d = PYB11TemplateFunction(convexHull2d, template_parameters="double")
+
+#...............................................................................
+@PYB11template("RealType")
+@PYB11implementation("""[](py::list& points,
+                           py::tuple& low,
+                           %(RealType)s& dx) {
+                               const auto coords = pybind11_helpers::copyCoords<3, %(RealType)s>(points);
+                               std::vector<%(RealType)s> vlow = {low[0].cast<%(RealType)s>(), 
+                                                                 low[1].cast<%(RealType)s>(),
+                                                                 low[2].cast<%(RealType)s>()};
+                               auto result = convexHull_3d(coords, &vlow.front(), dx);
+                               return result;
+                           }""")
+def convexHull3d(points = "py::list",
+                 low = "py::tuple",
+                 dx = "%(RealType)s&"):
+    """Return the convex hull of the points in a list of tuple positions of the form
+  points = [(x,y,z), (x,y,z), ...]
+  low: a tuple of coordinates (x,y,z) which should be less than any of the coordinates in "points"
+  dx:  the precision for distinguishing between coordinate values
+"""
+    return "PLC<3, %(RealType)s>"
+
+constructConvexHull3d = PYB11TemplateFunction(convexHull3d, template_parameters="double")
--- src/PYB11/polytope_pybind11_helpers.hh
+++ src/PYB11/polytope_pybind11_helpers.hh
@@ -15,6 +15,7 @@ namespace py = pybind11;
 namespace polytope {
 namespace pybind11_helpers {
 
+//------------------------------------------------------------------------------
 template<typename Key, typename Value>
 std::map<Key, std::vector<Value>>
 copyDictToMap(py::dict& pydict) {
@@ -22,9 +23,24 @@ copyDictToMap(py::dict& pydict) {
   for (const auto item: pydict) {
     const auto& key = item.first.cast<Key>();
     const auto& vallist = item.second.cast<py::list>();
-    result[key] = vector<Value>();
+    result[key] = std::vector<Value>();
     for (const auto vali: vallist) result[key].push_back(vali.cast<Value>());
   }
+  return result;
+}
+
+//------------------------------------------------------------------------------
+template<int Dimension, typename RealType>
+std::vector<RealType>
+copyCoords(const py::list& coords) {
+  std::vector<RealType> result;
+  for (const auto val: coords) {
+    const auto tup = val.cast<py::tuple>();
+    for (const auto vali: tup) {
+      result.push_back(vali.cast<RealType>());
+    }
+  }
+  POLY_ASSERT(result.size() % Dimension == 0);
   return result;
 }
 
